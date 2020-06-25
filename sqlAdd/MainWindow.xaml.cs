22c2d3c5c49753ab5c6af8306200d43134eae69a
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-06-26 00:45:22 +0200

    init wpf

---
+++ sqlAdd/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sqlAdd
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public class Coucou : ContentControl
+    {
+        public string Salutation { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+
+            //var b = new Button();
+            //b.Content = "Coucou";
+            //b.HorizontalAlignment = HorizontalAlignment.Left;
+            //b.Margin = new Thickness(204, 170, 0, 0);
+            //maGrille.Children.Add(b);
+        }
+    }
+}
